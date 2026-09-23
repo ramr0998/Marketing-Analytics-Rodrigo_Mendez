a8bc430c3aedbc84c9e5d23c7e31946475f0f407
ramr0998 <rodrigomendez0919@gmail.com>
2026-09-23 16:44:41 +0000

    Prueba 2

--- Semana_1/app.py
+++ Semana_1/app.py
@@ -2,14 +2,18 @@
 import streamlit as st
 from sklearn.linear_model import LinearRegression
 import numpy as np
+
 st.title("Configuracion inicial")
-st.write("Primera prueba de uso de streamlit y ambiente de MA2026")
-gasto=st.slider("Seleccine nivel de gasto en publicicdad", 10,200,50)
-variable_x = np.array([[10], [20], [30], [40],[50]])
-variable_y = np.array([15,25,35,45,55])
+st.write("Primera prueba de uso de Streamlit y ambiente de MA2026")
+
+gasto = st.slider("Seleccione nivel de gasto en publicidad", 10, 200, 50)
+
+variable_x = np.array([[10], [20], [30], [40], [50]])
+variable_y = np.array([15, 25, 35, 45, 55])
+
 modelo_lr = LinearRegression()
-modelo_lr.fit(variable_x,variable_y)
+modelo_lr.fit(variable_x, variable_y)
+
 if st.button("Predecir"):
- resultado = modelo_lr.predict([[gasto]])
- st.success(f"Las ventas proyectadas para una inversion de ${gasto} son:
-${resultado[0]}")
+    resultado = modelo_lr.predict([[gasto]])
+    st.success(f"Las ventas proyectadas para una inversion de ${gasto} son: ${resultado[0]}")
